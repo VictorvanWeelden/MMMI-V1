09ff46a5cfc2e7d9abe0c828256709f918c47f03
Anne van Terwisga <colibri.uxit@gmail.com>
2022-03-02 20:38:31 +0100

    Level completion duration tracking functional
    Time is tracked from start of SampleScene till player enters the finishfloor tile. Then the Game Over scene is loaded that displays the duration.

---
+++ MMMI-V1/Assets/GetTrackerData.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetTrackerData : MonoBehaviour
+{
+    public Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text.text = PlayerPrefs.GetString("playTime");
+    }
+}
