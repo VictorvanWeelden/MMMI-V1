5dbe6c6faff328f61d78d1ca261f9ea6ce7fb0b8
Anne van Terwisga <colibri.uxit@gmail.com>
2022-03-04 20:01:48 +0100

    Basic menu that gets and stores the username

---
+++ MMMI-V1/Assets/Scripts/Menu/GetUserName.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetUserName : MonoBehaviour
+{
+    InputField input;
+
+    public void GetInput() {
+        InputField input = GameObject.Find("User ID").GetComponent<InputField>();
+        PlayerPrefs.SetString("username", input.text);
+        Debug.Log(input.text);
+    }
+}
