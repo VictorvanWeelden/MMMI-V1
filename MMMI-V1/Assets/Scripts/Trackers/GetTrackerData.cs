daec633c895c979aaf558cdb6604ddc7f2ae8935
Anne van Terwisga <colibri.uxit@gmail.com>
2022-03-25 15:23:20 +0100

    Filename change + resize of menu & game over screen

--- MMMI-V1/Assets/Scripts/Trackers/GetTrackerData.cs
+++ MMMI-V1/Assets/Scripts/Trackers/GetTrackerData.cs
@@ -18,7 +18,22 @@ public class GetTrackerData : MonoBehaviour
     }
 
     void TakeScreenshot() {
-        string fileName = @"Assets\Screenshots\" + PlayerPrefs.GetString("username") + "_MAZETYPE_TimeErrs_" + System.DateTime.Now.ToString("dd-MM-yyyy-HH_mm_ss");
+        string fileName = Application.dataPath + "/Screenshots/" + System.DateTime.Now.ToString("dd-MM-yyyy-HH_mm_ss") + "_" + PlayerPrefs.GetString("username") + "_" + GetMazeType() + "_Level" + PlayerPrefs.GetInt("level") + "_TimeErrs_";
         ScreenCapture.CaptureScreenshot(fileName);
+    }
+
+    string GetMazeType(){
+        if (PlayerPrefs.GetInt("audio") == 1 && PlayerPrefs.GetInt("haptic") == 1){
+            return "COMBINED";
+        }
+        else if (PlayerPrefs.GetInt("audio") == 1) {
+            return "AUDIO";
+        }
+        else if (PlayerPrefs.GetInt("haptic") == 1) {
+            return "HAPTIC";
+        }
+        else {
+            return "UNKNOWNTYPE";
+        }
     }
 }
